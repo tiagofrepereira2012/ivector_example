8e21d55860fea13b02a1400af1de8da698145804
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2012-08-22 16:32:53 +0200

    tests

---
+++ simulations/attack_highdef_adverse_fixed/attack_highdef_adverse_fixed.sh
@@ -0,0 +1,30 @@
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_0 -rT 1 --patterns 0 --lda-machine /idiap/temp/tpereira/buildOutProjects/antispoofing.lbp/SIMULATIONS/BB_per_volume/VIDEO_GENERATOR/N888-R11N_circle/res_T1/lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine /idiap/temp/tpereira/buildOutProjects/antispoofing.lbp/SIMULATIONS/BB_per_volume/VIDEO_GENERATOR/N888-R11N_circle/res_T1/pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_1 -rT 1 --patterns 1
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_2 -rT 1 --patterns 2
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_3 -rT 1 --patterns 3
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_4 -rT 1 --patterns 4
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_5 -rT 1 --patterns 5
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_6 -rT 1 --patterns 6
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_7 -rT 1 --patterns 7
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_8 -rT 1 --patterns 8
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_9 -rT 1 --patterns 9
+
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R112_square -rT 2
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R113_square -rT 3
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R114_square -rT 4
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R115_square -rT 5
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square -rT 6 --patterns 9
+
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_0 -rT 6 --patterns 0
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_1 -rT 6 --patterns 1
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_2 -rT 6 --patterns 2
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_3 -rT 6 --patterns 3
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_4 -rT 6 --patterns 4
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_5 -rT 6 --patterns 5
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_6 -rT 6 --patterns 6
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_7 -rT 6 --patterns 7
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_8 -rT 6 --patterns 8
+#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R116_square_9 -rT 6 --patterns 9
+
