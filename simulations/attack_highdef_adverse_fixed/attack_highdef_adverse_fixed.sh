027298487e15628a7773691920d10995d0016b6e
Tiago Freitas Pereira <tiagofrepereira@gmail.com>
2012-08-22 18:57:24 +0200

    new version

--- simulations/attack_highdef_adverse_fixed/attack_highdef_adverse_fixed.sh
+++ simulations/attack_highdef_adverse_fixed/attack_highdef_adverse_fixed.sh
@@ -1,15 +1,23 @@
 ../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_0 -rT 1 --patterns 0 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
 
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_1 -rT 1 --patterns 1 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
 
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_1 -rT 1 --patterns 1
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_2 -rT 1 --patterns 2
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_3 -rT 1 --patterns 3
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_4 -rT 1 --patterns 4
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_5 -rT 1 --patterns 5
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_6 -rT 1 --patterns 6
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_7 -rT 1 --patterns 7
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_8 -rT 1 --patterns 8
-#../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_9 -rT 1 --patterns 9
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_2 -rT 1 --patterns 2 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_3 -rT 1 --patterns 3 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_4 -rT 1 --patterns 4 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_5 -rT 1 --patterns 5 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_6 -rT 1 --patterns 6 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_7 -rT 1 --patterns 7 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_8 -rT 1 --patterns 8 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
+../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R111_square_9 -rT 1 --patterns 9 --lda-machine lda_machine_0.99-XY-XT-YT-plane.txt --pca-machine pca_machine_0.99-XY-XT-YT-plane.txt --input-face-location attack_highdef_adverse_fixed.face --threshold 0.3730
+
 
 #../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R112_square -rT 2
 #../../../../bin/calclbptop_videos.py -i attack_highdef_adverse_fixed.mov -o R113_square -rT 3
